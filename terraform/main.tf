04e3227340fc360e71b149e7f71b172dd7cc58db
Ashan-Wijesinghe-997 <ashanbuddhika997@gmail.com>
2025-04-02 13:16:32 +0530

    Refactor Terraform configuration to update security group settings and enhance network interface attachment for stock predictor application

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ data "aws_instance" "existing_instance" {
 
 # Data source for the existing VPC
 data "aws_vpc" "existing_vpc" {
-  id = data.aws_instance.existing_instance.subnet.vpc_id
+  id = data.aws_instance.existing_instance.vpc_id
 }
 
 # Data source for the existing subnet
@@ -19,13 +19,43 @@ data "aws_subnet" "existing_subnet" {
 
 # Security group for the application
 resource "aws_security_group" "app_sg" {
-  # ... existing security group configuration ...
+  name        = "stock-predictor-sg"
+  description = "Security group for stock predictor application"
+  vpc_id      = data.aws_vpc.existing_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # Update the network interface with the new security group
-resource "aws_network_interface" "instance_eni" {
-  id = data.aws_instance.existing_instance.network_interface_id[0]
-  security_groups = [aws_security_group.app_sg.id]
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.app_sg.id
+  network_interface_id = data.aws_instance.existing_instance.network_interface_id
 }
 
 # Outputs
